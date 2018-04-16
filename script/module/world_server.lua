9aa2d58f398b2fd44e5c39b963a8fd2a29250c8a
mrq <362651591@qq.com>
2018-04-16 11:31:02 +0800

    update

--- script/module/world_server.lua
+++ script/module/world_server.lua
@@ -1,12 +1,17 @@
+local model = require "model"
 local database_common = import "module.database_common"
 
 
 function start(self)
-	
+	local db_channel = model.get_db_channel()
+	db_channel:db("common")
+	local guild_info = db_channel:findAll("guild")
+	for _,info in pairs(guild_info) do
+
+	end
 end
 
 function stop(self)
-
-
+	database_common:flush()
 end
 
