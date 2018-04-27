9c7ecbd33b3eec2a77effd37c6c5b6f9f7049813
mrq <362651591@qq.com>
2018-04-27 18:22:39 +0800

    update

--- shell/mongo_dump.sh
+++ shell/mongo_dump.sh
@@ -11,10 +11,10 @@ fi
 now=`date +%Y_%m_%d_%H_%M`
 for db in ${dbs[@]};
 do
-	mongodump -h 127.0.0.1:10105 -d $db -o ./mongo_bak/$db
-	tar zcvf ${db}_${now}.tar.gz  ./mongo_bak/$db/$db/
-	mv ${db}_${now}.tar.gz ./mongo_bak/$db/
-	rm -rf ./mongo_bak/$db/$db
+	mongodump -h 127.0.0.1:10105 -d $db -o ./mongo_bak/$now
+	tar zcvf ${db}.tar.gz  ./mongo_bak/$now/$db/
+	mv ${db}.tar.gz ./mongo_bak/$now/
+	rm -rf ./mongo_bak/$now/$db/
 done
 
 
