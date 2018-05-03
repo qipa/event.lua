172db746548828563a27369f41b2cd7a73305fe1
mrq <362651591@qq.com>
2018-05-03 13:27:22 +0800

    update

--- shell/commit.sh
+++ shell/commit.sh
@@ -2,25 +2,32 @@
 
 cd ..
 
-if [ -d "./data" ];then
-	rm -rf ./data
-fi
+# if [ -d "./data" ];then
+# 	rm -rf ./data
+# fi
 
-if [ -d "./log" ];then
-	rm -r ./log
-fi
+# if [ -d "./log" ];then
+# 	rm -r ./log
+# fi
 
-if [ -f "./data/master/dist_id" ];then
-	rm -rf ./data/master/dist_id
-fi
+# if [ -f "./data/master/dist_id" ];then
+# 	rm -rf ./data/master/dist_id
+# fi
 
-files=$(ls *.ipc 2> /dev/null | wc -l)
-if [ "$files" != "0" ]; then
-	rm ./*.ipc
-fi
+# files=$(ls *.ipc 2> /dev/null | wc -l)
+# if [ "$files" != "0" ]; then
+# 	rm ./*.ipc
+# fi
 
-make cleanall
+# make cleanall
 
-git add .
+rm -rf ./luaclib/convert/*.o
+rm -rf ./src/*.o
+
+git add ./luaclib/
+git add ./src
+git add ./lualib/
+git add ./script/
+git add ./shell/
 git commit -m "update"
 git push origin master
--- shell/mongo_start.sh
+++ shell/mongo_start.sh
@@ -3,10 +3,12 @@
 cd ..
 
 path=`pwd`
+conf_path=${path}/mongo_data/mongod.conf
 
 user=`whoami`
 
-result=`ps -U${user}|grep -c mongod`
+# result=`ps -U${user} -elf|grep mongod|grep -v grep|grep -c ${conf_path}`
+result=$(ps -U${user} -elf|grep mongod|grep ${conf_path}|grep -v grep|wc -l)
 
 if [ $result -eq 0 ];then
 	if [ ! -d "./mongo_data" ];then
@@ -25,9 +27,10 @@ if [ $result -eq 0 ];then
 		echo "logappend=true" >> $mongod_conf
 		echo "fork=true" >> $mongod_conf
 		echo "port=10105" >> $mongod_conf
+		echo "pidfilepath=${path}/mongo_data/mongod.pid" >> $mongod_conf
 	fi
 
-	mongod -f ./mongo_data/mongod.conf &
+	mongod -f ${path}/mongo_data/mongod.conf &
 else
 	pid=`ps -U${user}|grep mongod|awk '{print $1}'`
 	echo "mongod:${pid} already running"
