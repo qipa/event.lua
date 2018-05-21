01387fe060d0f601686d8de41087472ad6c12698
mrq <362651591@qq.com>
2018-05-21 16:56:36 +0800

    update

--- script/test_filter.lua
+++ script/test_filter.lua
@@ -37,7 +37,7 @@ event.error(string.format("lua mem:%fkb,c mem:%fkb",lua_mem,helper.allocated()/1
 -- end)
 
 -- table.print(filter0.split("1.3。‘几个简单的命令5"))
-print(filter0.convert(".","utf-8","gb2312"))
+print(filter0.convert("adfasfd","gb2312","utf-8"))
 -- collectgarbage("collect")
 
 -- local filter_inst1 = filter1.create()
