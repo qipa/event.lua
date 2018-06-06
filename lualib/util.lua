0db588c55a0d3bc11509fe32f1dbc77463bc8b76
mrq <362651591@qq.com>
2018-06-06 13:25:16 +0800

    update

--- lualib/util.lua
+++ lualib/util.lua
@@ -252,8 +252,100 @@ function _M.decimal_sub(value,from,to)
     return l
 end
 
-function _M.utf8_length(str)
-    utf8.codepoint(str,2,3)
+--vector2
+function _M.normalize(x,z)
+    local dt = math.sqrt(x * x + z * z)
+    return x / dt, z / dt
+end
+
+function _M.lerp(from_x,from_z,to_x,to_z,ratio)
+    return from_x + (to_x - from_x) * ratio,from_z + (to_z - from_z) * ratio
+end
+
+function _M.distance(from_x,from_z,to_x,to_z)
+    return math.sqrt((from_x - to_x)^2 + (from_z - to_z)^2)
+end
+
+function _M.move_forward(from_x,from_z,to_x,to_z,pass)
+    local distance = _M.distance(from_x,from_z,to_x,to_z)
+    local ratio = pass / distance
+    if ratio > 1 then
+        ratio = 1
+    end
+    return _M.lerp(from_x,from_z,to_x,to_z,ratio)
+end
+
+function _M.move_toward(from_x,from_z,dir,distance)
+    local radian = math.atan2(dir.z / dir.x)
+    local x = math.cos(radian) * distance + from_x
+    local z = math.sin(radian) * distance + from_z
+    return x,z
+end
+
+function _M.rotation(x,z,center_x,center_z,angle)
+    local radian = math.rad(angle)
+    local sin = math.sin(radian)
+    local cos = math.cos(radian)
+    local rx = (x - center_x) * cos - (z - center_z) * sin + center_x
+    local rz = (x - center_x) * sin - (z - center_z) * cos + center_z
+    return rx,rz
+end
+
+function _M.inside_circle(src_x,src_z,range,x,z)
+    local dx = src_x - x
+    local dz = src_z - z
+
+    if math.abs(dx) > range or math.abs(dz) > range then
+        return false
+    end
+    return dx * dx + dz * dz <= range * range
+end
+
+function _M.inside_sector(src_x,src_z,range,toward_angle,degree,x,z)
+    if not _M.inside_circle(src_x,src_z,range,x,z) then
+        return false
+    end
+
+    local dx = x - src_x
+    local dz = z - src_z
+
+    local z_angle = math.deg(math.atan2(dx,dz)) - toward_angle + degree / 2
+
+    while z_angle > 360 do
+        z_angle = z_angle - 360
+    end
+
+    while z_angle < 0 do
+        z_angle = z_angle + 360
+    end
+
+    return z_angle <= degree
+end
+
+function _M.inside_rectangle(src_x,src_z,toward_angle,length,width,x,z)
+    local dx = x - src_x
+    local dz = z - src_z
+
+    local z_angle = math.deg(math.atan2(dx,dz)) - toward_angle
+    if z_angle >= 270 then
+        z_angle = z_angle - 360
+    elseif z_angle <= -270 then
+        z_angle = z_angle + 360
+    end
+
+    if z_angle < -90 or z_angle > 90 then
+        return false
+    end
+
+    local z_radian = math.rad(math.abs(z_angle))
+    local dt = math.sqrt(dx * dx + dz * dz)
+    local x_change = dt * math.cos(z_radian)
+    local z_change = dt * math.sin(z_radian)
+
+    if (x_change < 0 or x_change > length) or (z_change < 0 or z_change > width) then
+        return false
+    end
+    return true
 end
 
 return _M
